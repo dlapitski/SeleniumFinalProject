8f6aaa7fe05cbab70200c99c56bc3bef51c34b8c
Dzmitry Lapitski <dz.lap.pro@gmail.com>
2023-03-27 15:08:37 +0200

    Reverted commit - push again: Added 'Base' page locators for login link, moved methods for login link and 'Login' page from 'Main' page to 'Base' page, added new tests for 'Product' page to test the login link and 'Login' page

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 import pytest
 
 
@@ -17,6 +18,7 @@ def test_guest_can_add_product_to_basket(browser, promo_link):
     page.should_be_correct_price_in_basket()    # проверяем корректность цены добавленного товара в корзину
 
 @pytest.mark.xfail
+@pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = 'https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/'
     page = ProductPage(browser, link)       # иниц. Page Object model для product page, задаем browser и link
@@ -24,6 +26,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.add_to_basket()                    # добавляем товар в корзину
     page.should_not_be_success_message()    # не должно быть success message после добавления товара в корзину
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     link = 'https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/'
     page = ProductPage(browser, link)       # иниц. Page Object model для product page, задаем browser и link
@@ -31,9 +34,24 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()    # проверяем отсутствие success message если товар еще не в корзине
 
 @pytest.mark.xfail
+@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = 'https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/'
     page = ProductPage(browser, link)           # иниц. Page Object model для product page, задаем browser и link
     page.open()                                 # открываем product page
     page.add_to_basket()                        # добавляем товар в корзину
     page.should_success_message_disappear()     # success message должен пропасть со страницы
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
